d87ebc6c74308e305d8fd4513b20d2e86ee3b042
Brian Hart <brian.hart@xylogix.net>
2021-02-10 22:44:16 -0500

    Create UrlPaginator and ApiPaginator values

--- xyLOGIX.Data.Paginators.Factories/GetPaginator.cs
+++ xyLOGIX.Data.Paginators.Factories/GetPaginator.cs
@@ -8,7 +8,16 @@ namespace xyLOGIX.Data.Paginators.Factories
    /// </summary>
    public enum PaginatorType
    {
-      // TODO: Implement additional enum members here.
+      /// <summary>
+      /// Moves between pages merely by altering the request route (URL).
+      /// </summary>
+      UrlPaginator,
+
+      /// <summary>
+      /// Moves between the pages of data returned by an API by making special
+      /// calls to methods.
+      /// </summary>
+      ApiPaginator,
 
       /// <summary>
       /// Unknown strategy type.
@@ -17,17 +26,48 @@ namespace xyLOGIX.Data.Paginators.Factories
    }
 
    /// <summary>
-   /// Creates instances of objects that implement the <see cref="T:xyLOGIX.Data.Paginators.Interfaces.IPaginator"/> interface.
+   /// Creates instances of objects that implement the
+   /// <see
+   ///    cref="T:xyLOGIX.Data.Paginators.Interfaces.IPaginator" />
+   /// interface.
    /// </summary>
    public static class GetPaginator
    {
       /// <summary>
-      /// Creates an instance of an object implementing the <see cref="T:xyLOGIX.Data.Paginators.Interfaces.IPaginator"/> interface which corresponds to the value specified in <paramref name="type"/>, and returns a reference to it.
+      /// Creates an instance of an object implementing the
+      /// <see
+      ///    cref="T:xyLOGIX.Data.Paginators.Interfaces.IPaginator" />
+      /// interface
+      /// which corresponds to the value specified in <paramref name="type" />,
+      /// and returns a reference to it.
       /// </summary>
-      /// <param name="type">One of the <see cref="T:xyLOGIX.Data.Paginators.Factories.PaginatorType"/> values that describes what type of object you want.</param>
-      /// <returns>A reference to the instance of the object that implements the <see cref="T:xyLOGIX.Data.Paginators.Interfaces.IPaginator"/> interface which corresponds to the value specified in <paramref name="type"/>.</returns>
-      /// <remarks>This method will throw an exception if there are no types implemented that correspond to the value of <paramref name="type"/>.</remarks>
-      /// <exception cref="T:System.ArgumentOutOfRangeException">Thrown if there is no corresponding concrete type defined that implements the <see cref="T:xyLOGIX.Data.Paginators.Interfaces.IPaginator"/> interface and which corresponds to the value passed in the <paramref name="type"/> parameter.</exception>
+      /// <param name="type">
+      /// One of the
+      /// <see
+      ///    cref="T:xyLOGIX.Data.Paginators.Factories.PaginatorType" />
+      /// values that
+      /// describes what type of object you want.
+      /// </param>
+      /// <returns>
+      /// A reference to the instance of the object that implements the
+      /// <see
+      ///    cref="T:xyLOGIX.Data.Paginators.Interfaces.IPaginator" />
+      /// interface
+      /// which corresponds to the value specified in <paramref name="type" />.
+      /// </returns>
+      /// <remarks>
+      /// This method will throw an exception if there are no types implemented
+      /// that correspond to the value of <paramref name="type" />.
+      /// </remarks>
+      /// <exception cref="T:System.ArgumentOutOfRangeException">
+      /// Thrown if there is no corresponding concrete type defined that
+      /// implements the
+      /// <see
+      ///    cref="T:xyLOGIX.Data.Paginators.Interfaces.IPaginator" />
+      /// interface and
+      /// which corresponds to the value passed in the <paramref name="type" />
+      /// parameter.
+      /// </exception>
       public static IPaginator For(PaginatorType type)
       {
          IPaginator result;

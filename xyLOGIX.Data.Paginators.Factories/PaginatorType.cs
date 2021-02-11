c82567f5b49097b091dbfe6cb2c8954461ba035e
Brian Hart <brian.hart@xylogix.net>
2021-02-10 22:53:28 -0500

    Move types into matching files

---
+++ xyLOGIX.Data.Paginators.Factories/PaginatorType.cs
@@ -0,0 +1,24 @@
+﻿namespace xyLOGIX.Data.Paginators.Factories
+{
+   /// <summary>
+   /// Values describing the type of Paginator you want.
+   /// </summary>
+   public enum PaginatorType
+   {
+      /// <summary>
+      /// Moves between pages merely by altering the request route (URL).
+      /// </summary>
+      UrlPaginator,
+
+      /// <summary>
+      /// Moves between the pages of data returned by an API by making special
+      /// calls to methods.
+      /// </summary>
+      ApiPaginator,
+
+      /// <summary>
+      /// Unknown strategy type.
+      /// </summary>
+      Unknown = -1
+   }
+}

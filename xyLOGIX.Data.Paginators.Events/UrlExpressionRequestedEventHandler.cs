1ade3b11c0fa810b3c5e197813d51ae6361c4647
Brian Hart <brian.hart@xylogix.net>
2023-12-11 10:07:44 -0700

    Create UrlExpressionRequestedEventHandler.cs

---
+++ xyLOGIX.Data.Paginators.Events/UrlExpressionRequestedEventHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace xyLOGIX.Data.Paginators.Events
+{
+    /// <summary>
+    /// Method signature for the handler of a <c>UrlExpressionRequested</c> event.
+    /// </summary>
+    /// <param name="sender">(Required.) Reference to the sender of the event.</param>
+    /// <param name="e">
+    /// (Required.) A
+    /// <see cref="T:xyLOGIX.Data.Paginators.Events.UrlExpressionRequestedEventArgs" />
+    /// containing the event data.
+    /// </param>
+    /// <returns></returns>
+    public delegate string UrlExpressionRequestedEventHandler(
+        object sender,
+        UrlExpressionRequestedEventArgs e
+    );
+}

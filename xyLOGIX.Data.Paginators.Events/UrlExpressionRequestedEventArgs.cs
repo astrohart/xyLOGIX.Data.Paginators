1bc4a39fcc5009cb8815e4ba8a459a44b734fc53
Brian Hart <brian.hart@xylogix.net>
2023-12-11 10:07:04 -0700

    Create UrlExpressionRequestedEventArgs.cs

---
+++ xyLOGIX.Data.Paginators.Events/UrlExpressionRequestedEventArgs.cs
@@ -0,0 +1,41 @@
+﻿using PostSharp.Patterns.Threading;
+using System;
+
+namespace xyLOGIX.Data.Paginators.Events
+{
+    /// <summary>
+    /// Information for a <c>UrlExpressionRequested</c> event.
+    /// </summary>
+    [ExplicitlySynchronized]
+    public class UrlExpressionRequestedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see cref="T:xyLOGIX.Data.Paginators.Events.UrlExpressionRequestedEventArgs" /> and
+        /// returns a reference to it.
+        /// </summary>
+        /// <param name="currentPage">
+        /// (Required.) Integer value specifying the number of the current page of data
+        /// that is to be requested from the server.
+        /// </param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException">
+        /// Thrown if a negative quantity is passed for the value of the
+        /// <paramref name="currentPage" /> parameter.
+        /// </exception>
+        public UrlExpressionRequestedEventArgs(int currentPage)
+        {
+            if (currentPage < 0)
+                throw new ArgumentOutOfRangeException(
+                    nameof(currentPage),
+                    "Current page cannot be a negative number."
+                );
+
+            CurrentPage = currentPage;
+        }
+
+        /// <summary>
+        /// Gets an integer that is set to the number of the current page.
+        /// </summary>
+        public int CurrentPage { get; }
+    }
+}
